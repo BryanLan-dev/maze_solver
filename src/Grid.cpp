4136f9306773be42ed0dc58969fdcbc4b267ce27
BryanLan-dev <2006landers@gmail.com>
2026-08-09 20:30:39 -0500

    Near the end of the Grid.cpp file. Work that needs to be done: finish pop/backtrack + pick neighbor in void generate(), then move onto Game, DrawText, and Main. Hopefully finish the maze by tomorrow.

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,9 +1,72 @@
-#include <chrono>
-#include <random>
 #include <stack>
 #include <SFML/Graphics.hpp>
-
 #include "Headers/Cell.hpp"
-#include "Headers/Global.hpp"
 #include "Headers/RandomManager.hpp"
 #include "Headers/Grid.hpp"
+
+
+
+//Constructor to initialize the grid with a reference to the RandomManager instance
+Grid::Grid(RandomManager& i_random_manager):
+    random_manager(i_random_manager)
+{
+    for(unsigned short y =0; y < height; ++y)
+    {
+        std::vector<Cell> row;
+        for(unsigned short x = 0; x < width; ++x)
+        {
+            row.emplace_back(x, y);
+        }
+        cells.push_back(row);
+    }
+}
+
+//Generate the maze using a depth-first search algorithm
+void Grid::generate()
+{
+    //Stack to hold the path of the maze generation
+    std::stack<Cell*> path;
+
+    cells[0][0].set_visited(true);
+
+    path.push(&cells[0][0]);
+    
+    //While there are still cells in the path stack, continue generating the maze
+    while (!path.empty())
+    {
+        Cell* current_cell = path.top();
+
+        std::vector<Cell*> unvisited_neighbors;
+        
+        if(current_cell->get_x() > 0 && !cells[current_cell->get_y()][current_cell->get_x() - 1].get_visited())
+        {
+            unvisited_neighbors.push_back(&cells[current_cell->get_y()][current_cell->get_x() - 1]);
+        }
+
+
+        if(current_cell->get_x() < width - 1 && !cells[current_cell->get_y()][current_cell->get_x() + 1].get_visited())
+        {
+            unvisited_neighbors.push_back(&cells[current_cell->get_y()][current_cell->get_x() + 1]);
+        }
+
+
+        if(current_cell->get_y() > 0 && !cells[current_cell->get_y() - 1][current_cell->get_x()].get_visited())
+        {
+        unvisited_neighbors.push_back(&cells[current_cell->get_y() - 1][current_cell->get_x()]);
+        }
+
+
+        if(current_cell->get_y() < height - 1 && !cells[current_cell->get_y() + 1][current_cell->get_x()].get_visited())
+        {
+            unvisited_neighbors.push_back(&cells[current_cell->get_y() + 1][current_cell->get_x()]);
+        }
+
+
+       /* if(!unvisited_neighbors.empty())
+        {
+        
+        }
+        */
+    }
+
+}
