1e2ff2d1a0ba57419daf7faa08d47f3281d115a5
BryanLan-dev <2006landers@gmail.com>
2026-08-09 19:25:59 -0500

    Finished making Grid class members in the hpp file. Now time to actually write the code for the cpp file.

--- src/Headers/Grid.hpp
+++ src/Headers/Grid.hpp
@@ -1,11 +1,27 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Cell.hpp"
+#include "RandomManager.hpp"
 #include <vector>
-#include <stack>
 
-//
+
 class Grid{
+    //2D vector to hold the cells of the grid
+    std::vector<std::vector<Cell>> cells;
+    
+    //Width and height of the grid
+    static constexpr unsigned short width = 20;
+    static constexpr unsigned short height = 20;
 
+    
+    //Reference to the RandomManager instance for generating random numbers
+    RandomManager& random_manager;
+    
+    //Sprite and texture for rendering the cells
+    sf::Sprite cell_sprite;
+	sf::Texture cell_texture;
 
-
+    public:
+    Grid(RandomManager& i_random_manager);
+    void generate();
 };
