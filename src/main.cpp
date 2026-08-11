347a75bc51dbbe4e092e51313e4d98f4edf7f7cb
BryanLan-dev <2006landers@gmail.com>
2026-08-11 00:03:22 -0500

    Just finished building the project .

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,14 @@
-#include <chrono>
-#include <random>
-#include <stack>
 #include <SFML/Graphics.hpp>
-
-#include "Headers/Cell.hpp"
-#include "Headers/Global.hpp"
 #include "Headers/RandomManager.hpp"
 #include "Headers/Grid.hpp"
 #include "Headers/Game.hpp"
 
-
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    RandomManager random_manager;
+    Grid grid(random_manager);
+    grid.generate();
+    Game game(grid);
+    game.run();
 
 }
